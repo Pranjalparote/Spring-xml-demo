269dc03fde86bcc6827d6a52263d6a7097f7532f
pranjal <pranjalarote96@gmail.com>
2019-05-14 14:40:32 +0530

    Task2 pushed

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -5,6 +5,15 @@ public class Actor {
     private String gender;
     private int age;
 
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     Actor() {
 
     }
@@ -16,36 +25,5 @@ public class Actor {
     }
 
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Actor{" +
-                "name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,6 @@
 
-
 package com.stackroute.domain;
+
 
 public class Movie {
     Actor actor;
@@ -10,22 +10,15 @@ public class Movie {
 
     }
 
-    public Movie(Actor actor) {
-        this.actor = actor;
-    }
-
-    public Actor getActor() {
-        return actor;
-    }
-
-    public void setActor(Actor actor) {
-        this.actor = actor;
-    }
-
 
     @Override
     public String toString() {
-        return "Actor Name: " + actor.getName() + "\n" + "Acter gender:" + actor.getGender() + "\n" + "Acter age:" + actor.getAge();
+        return "ActorName=" + actor;
+    }
+
+    public Movie(Actor actor) {
+        this.actor = actor;
     }
 }
+
 
